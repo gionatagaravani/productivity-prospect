ac4c52a83efe9a9a726a458720c80e20d2b8cecb
Gionata Garavani <gionatagaravani@libero.it>
2024-11-29 17:23:01 +0100

    fix pipeline

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const { withContentlayer } = require('next-contentlayer')
-const path = require('path')
-const fs = require('fs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,23 +7,6 @@ const nextConfig = {
     domains: [],
     unoptimized: true,
   },
-  // Copy .htaccess to output directory during build
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.plugins.push({
-        apply: (compiler) => {
-          compiler.hooks.afterEmit.tap('CopyHtaccess', () => {
-            const publicHtaccess = path.join(__dirname, 'public', '.htaccess')
-            const outputHtaccess = path.join(__dirname, 'out', '.htaccess')
-            if (fs.existsSync(publicHtaccess)) {
-              fs.copyFileSync(publicHtaccess, outputHtaccess)
-            }
-          })
-        }
-      })
-    }
-    return config
-  }
 }
 
 module.exports = withContentlayer(nextConfig)
