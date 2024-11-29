54f184733305cec66568d3c89bee0cee4e3b9a9c
Gionata Garavani <gionatagaravani@libero.it>
2024-11-29 13:07:17 +0100

    okok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Blog', href: '/blog' },
   { name: 'About', href: '/about' },
-  { name: 'Contact', href: '/contact' },
 ]
 
 export default function Header() {
