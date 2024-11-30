732a398f6cc205603ebcc323158e7a968d28d0e0
Gionata Garavani <gionatagaravani@libero.it>
2024-11-30 17:53:56 +0100

    build

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
   images: {
     domains: [],
     unoptimized: true,
-  },
-  // Add this to ensure proper static HTML generation
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/404': { page: '/404' },
-      // Add other static routes here if needed
-    }
   }
 }
 
